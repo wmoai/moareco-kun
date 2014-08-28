7852077184eaf9910475fa7eee1717d641385eb8
t_watase <t_watase@okwave.co.jp>
2014-08-28 22:23:36 +0900

    Fix log path, error regex

--- observer.js
+++ observer.js
@@ -10,7 +10,7 @@ var size = fs.statSync(path).size;
 // observer
 fs.watch(path, function(event, filename) {
   if (size < fs.statSync(path).size) {
-    var readableStream = fs.createReadStream('message.txt', {start:size}, {encoding: 'utf-8', bufferSize: 1});
+    var readableStream = fs.createReadStream(path, {start:size}, {encoding: 'utf-8', bufferSize: 1});
     size = fs.statSync(path).size;
     readableStream.on('data', function(data) {
       detect(data.toString().trim());
@@ -23,8 +23,9 @@ fs.watch(path, function(event, filename) {
 
 // twitter
 var detect = function(line) {
-  var message = line.match(/\[[^\[\]]+\] \[[^\[\]]+\] \S+ - (.+)/)[1];
-  if (message) {
+  var match = line.match(/\[[^\[\]]+\] \[([^\[\]]+)\] \S+ - (.+)/);
+  if (match[1] == 'ERROR') {
+    var message = match[2];
     twit.verifyCredentials(function(data) {
     }).updateStatus("エラーでた : "+message, function(data) {
     });
