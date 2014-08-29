e1ec1f9e1da8d16bedae264a98f536d3177821b1
t_watase <t_watase@okwave.co.jp>
2014-08-29 15:43:18 +0900

    Add reconnecting for missing file, repling repoter

--- observer.js
+++ observer.js
@@ -2,24 +2,39 @@ var fs = require('fs');
 var config = require('config');
 var util = require('util')
   , twitter = require('twitter');
-var twit = new twitter(config.Twitter);
+var twit = new twitter(config.Twitter.keys);
 
 var path = config.Log.path;
 var size = fs.statSync(path).size;
 
 // observer
-fs.watch(path, function(event, filename) {
-  if (size < fs.statSync(path).size) {
-    var readableStream = fs.createReadStream(path, {start:size}, {encoding: 'utf-8', bufferSize: 1});
-    size = fs.statSync(path).size;
-    readableStream.on('data', function(data) {
-      detect(data.toString().trim());
-    });
-    readableStream.on('end', function() {
+var observeLog = function() {
+  console.log('Connect.');
+  var observer = fs.watch(path, function(event, filename) {
+    switch (event) {
+    case 'change':
+      if (size < fs.statSync(path).size) {
+        var readableStream = fs.createReadStream(path, {start:size}, {encoding: 'utf-8', bufferSize: 1});
+        size = fs.statSync(path).size;
+        readableStream.on('data', function(data) {
+          detect(data.toString().trim());
+        });
+        readableStream.on('end', function() {
 
-    });
-  }
-});
+        });
+      }
+      break;
+    case 'rename':
+      observer.close();
+      console.log('File was renamed, reconnect in 3sec.');
+      setTimeout(function() {
+        observeLog();
+      }, 3000);
+      break;
+    }
+  });
+}
+observeLog();
 
 // twitter
 var detect = function(line) {
@@ -27,7 +42,7 @@ var detect = function(line) {
   if (match[1] == 'ERROR') {
     var message = match[2];
     twit.verifyCredentials(function(data) {
-    }).updateStatus("エラーでた : "+message, function(data) {
+    }).updateStatus("@"+config.Twitter.reporter+" エラーでた : "+message, function(data) {
     });
   }
 }
